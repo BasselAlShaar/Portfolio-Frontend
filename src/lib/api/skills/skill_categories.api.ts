69bddb7babad475c3cf1e585087b5ca27340af06
Bassel Al Shaar <basselalshaar11@gmail.com>
2026-09-05 19:49:38 +0300

    feat(api): add skill categories api support

---
+++ src/lib/api/skills/skill_categories.api.ts
@@ -0,0 +1,18 @@
+import { api } from "../client";
+import type {
+    SkillsCategoryData,
+    CreateSkillsCategoryData,
+    UpdateSkillsCategoryData
+} from "@/lib/types";
+
+export const educationApi = {
+    list: () => api.get<SkillsCategoryData[]>(`/admin/skills/categories`),
+
+    get: (id: string) => api.get<SkillsCategoryData>(`/admin/skills/categories${id}`),
+
+    create: (data: CreateSkillsCategoryData) => api.post<SkillsCategoryData>("/admin/skills/categories", data),
+
+    update: (id:string, data: UpdateSkillsCategoryData) => api.patch<SkillsCategoryData>(`/admin/skills/categories${id}`, data),
+
+    delete: (id: string) => api.delete<{ id: string | null }>(`/admin/skills/categories${id}`),
+}
