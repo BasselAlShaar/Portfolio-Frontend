17231633988eb4297cf30f5ac609cb92bf7cf966
Bassel Al Shaar <basselalshaar11@gmail.com>
2026-09-05 19:51:39 +0300

    feat(api): add technologies categories api support

---
+++ src/lib/api/technologies/technology_categories.api.ts
@@ -0,0 +1,18 @@
+import { api } from "../client";
+import type {
+    TechnologiesCategoryData,
+    CreateTechnologiesCategoryData,
+    UpdateTechnologiesCategoryData
+} from "@/lib/types";
+
+export const educationApi = {
+    list: () => api.get<TechnologiesCategoryData[]>(`/admin/technologies/categories`),
+
+    get: (id: string) => api.get<TechnologiesCategoryData>(`/admin/technologies/categories${id}`),
+
+    create: (data: CreateTechnologiesCategoryData) => api.post<TechnologiesCategoryData>("/admin/technologies/categories", data),
+
+    update: (id:string, data: UpdateTechnologiesCategoryData) => api.patch<TechnologiesCategoryData>(`/admin/technologies/categories${id}`, data),
+
+    delete: (id: string) => api.delete<{ id: string | null }>(`/admin/technologies/categories${id}`),
+}
