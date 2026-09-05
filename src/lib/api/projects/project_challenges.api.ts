8926d38d900ea9426701b4c1fbb585e889a35316
Bassel Al Shaar <basselalshaar11@gmail.com>
2026-09-05 19:45:09 +0300

    feat(api): add project challenges api support

---
+++ src/lib/api/projects/project_challenges.api.ts
@@ -0,0 +1,18 @@
+import { api } from "../client";
+import type {
+    ProjectChallengesData,
+    CreateProjectChallengesData,
+    UpdateProjectChallengesData
+} from "@/lib/types";
+
+export const educationApi = {
+    list: (slug: string) => api.get<ProjectChallengesData[]>(`/admin/projects/challenges/all/${slug}`),
+
+    get: (id: string) => api.get<ProjectChallengesData>(`/admin/projects/challenges${id}`),
+
+    create: (data: CreateProjectChallengesData) => api.post<ProjectChallengesData>("/admin/projects/challenges", data),
+
+    update: (id:string, data: UpdateProjectChallengesData) => api.patch<ProjectChallengesData>(`/admin/projects/challenges${id}`, data),
+
+    delete: (id: string) => api.delete<{ id: string | null }>(`/admin/projects/challenges${id}`),
+}
