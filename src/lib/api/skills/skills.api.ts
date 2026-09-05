40abca14bbe8a8a014131ab36f0773d602015713
Bassel Al Shaar <basselalshaar11@gmail.com>
2026-09-05 19:47:30 +0300

    feat(api): add skills api support

---
+++ src/lib/api/skills/skills.api.ts
@@ -0,0 +1,18 @@
+import { api } from "../client";
+import type {
+    SkillsData,
+    CreateSkillsData,
+    UpdateSkillsData
+} from "@/lib/types";
+
+export const educationApi = {
+    list: () => api.get<SkillsData[]>("/admin/projects/skills"),
+
+    get: (id: string) => api.get<SkillsData>(`/admin/skills${id}`),
+
+    create: (data: CreateSkillsData) => api.post<SkillsData>("/admin/skills", data),
+
+    update: (id:string, data: UpdateSkillsData) => api.patch<SkillsData>(`/admin/skills${id}`, data),
+
+    delete: (id: string) => api.delete<{ id: string | null }>(`/admin/skills${id}`),
+}
