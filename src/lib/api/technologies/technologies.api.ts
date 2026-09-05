cd047e52da12936cd343ef087e9b776d98908f75
Bassel Al Shaar <basselalshaar11@gmail.com>
2026-09-05 19:50:38 +0300

    feat(api): add technologies api support

---
+++ src/lib/api/technologies/technologies.api.ts
@@ -0,0 +1,18 @@
+import { api } from "../client";
+import type {
+    TechnologiesData,
+    CreateTechnologiesData,
+    UpdateTechnologiesData
+} from "@/lib/types";
+
+export const educationApi = {
+    list: () => api.get<TechnologiesData[]>(`/admin/technologies`),
+
+    get: (id: string) => api.get<TechnologiesData>(`/admin/technologies${id}`),
+
+    create: (data: CreateTechnologiesData) => api.post<TechnologiesData>("/admin/technologies", data),
+
+    update: (id:string, data: UpdateTechnologiesData) => api.patch<TechnologiesData>(`/admin/technologies${id}`, data),
+
+    delete: (id: string) => api.delete<{ id: string | null }>(`/admin/technologies${id}`),
+}
